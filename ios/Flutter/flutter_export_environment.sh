8889ebbece9cfb83fa13bd4f2ba25aa81c648b6b
Rustam Usmanov <rustamusmanov708@gmail.com>
2022-01-13 05:37:48 +0500

    package implemented

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/rustamusmanov/fvm/versions/1.22.0"
+export "FLUTTER_APPLICATION_PATH=/Users/rustamusmanov/Desktop/location"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/rustamusmanov/fvm/versions/1.22.0/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=4.3.0"
+export "FLUTTER_BUILD_NUMBER=4.3.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
